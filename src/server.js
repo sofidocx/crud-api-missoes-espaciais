8bc222dcee476f4f7515232b6b6240f8880f7380
Sofia Vaz <sofiavazdacostaxavier@gmail.com>
2025-08-20 17:10:03 -0300

    feat: conecta banco de dados e cria tabela principal de missões espaciais

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 const express = require('express'); 
+const db = require('./database/db.js'); 
+
 const app = express(); 
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+// inicializando o banco de dados 
+
+db.serialize(() => {
+    console.log('Banco de dados e tabelas prontos!'); 
+}); 
+
 
 app.get('/', (req, res) => {
     res.send('API de missões espaciais! Servidor rodando com sucesso'); 
 }); 
 
 const PORT = 3000; 
+
+// servidor escutando na porta declarada 
 
 app.listen(PORT, () => {
     console.log(`Servidor está rodando na porta ${PORT}.
